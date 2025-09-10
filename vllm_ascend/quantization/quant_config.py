0d66d206bded28e6fef154d8665d2159c2ee5f67
moguizhizi <moguizhiz@126.com>
2025-09-10 09:01:37 +0000

    should_ignore_layer

--- vllm_ascend/quantization/quant_config.py
+++ vllm_ascend/quantization/quant_config.py
@@ -165,6 +165,87 @@ class AscendQuantConfig(QuantizationConfig):
                 return mapped_name.rsplit(".", 1)[0] + "." + proj_name
 
         return prefix
+    
+    def _is_equal_or_regex_match(self, value: str,
+                             target: str,
+                             check_contains: bool = False) -> bool:
+        """
+        Checks whether a value is exactly equal or a regex match for target
+        if target starts with 're:'. If check_contains is set to True,
+        additionally checks if the target string is contained within the value.
+        """
+
+        if target.startswith("re:"):
+            pattern = target[3:]
+            if re.match(pattern, value):
+                return True
+        elif check_contains:
+            if target.lower() in value.lower():
+                return True
+        elif target == value:
+            return True
+        return False
+    
+    def check_equal_or_regex_match(self, layer_name: str,
+                               targets: Iterable[str] = tuple(),) -> bool:
+        """
+        Checks whether a layer_name is exactly equal or a regex match for
+        if target starts with 're:' to any target in list.
+        """
+        for target in targets:
+            if self._is_equal_or_regex_match(layer_name, target):
+                return True
+        return False
+    
+    def should_ignore_layer(self, layer_name: Optional[str], 
+                            ignore: Iterable[str] = tuple(),
+                            fused_mapping: Mapping[str, list[str]] = MappingProxyType({})) -> bool:
+    
+        if layer_name is None:
+            return False
+
+        # layer_name = model.layers.0.self_attn.qkv_proj
+        # proj_name = qkv_proj
+        proj_name = layer_name.split(".")[-1]
+
+        # Fused layers like gate_up_proj or qkv_proj will not be fused
+        # in the safetensors checkpoint. So, we convert the name
+        # from the fused version to unfused + check to make sure that
+        # each shard of the fused layer has the same scheme.
+        if proj_name in fused_mapping and layer_name not in ignore:
+            shard_proj_names = fused_mapping[proj_name]
+
+            # Convert fused_name --> [shard_names]
+            shard_names = [
+                layer_name.replace(proj_name, shard_proj_name)
+                for shard_proj_name in shard_proj_names
+            ]
+
+            # Layer should be ignored if shards are ignored.
+            should_ignore_layer = None
+            for shard_name in shard_names:
+                should_ignore_shard =self.check_equal_or_regex_match(
+                    layer_name=shard_name, targets=ignore)
+
+                # If shard_idx=0, set layer ignore to match shard.
+                if should_ignore_layer is None:
+                    should_ignore_layer = should_ignore_shard
+
+                # If shard_idx=1+ confirm scheme matches prior shards.
+                elif should_ignore_shard != should_ignore_layer:
+                    raise ValueError(
+                        f"Detected some but not all shards of {proj_name} "
+                        "are quantized. All shards of fused layers "
+                        "to have the same precision.")
+
+        # Unfused layers like down_proj and o_proj will match
+        # the safetensors checkpoint already.
+        else:
+            should_ignore_layer = self.check_equal_or_regex_match(layer_name=layer_name,
+                                                            targets=ignore)
+
+        assert should_ignore_layer is not None
+        return should_ignore_layer
 
     def get_quant_method(self, layer: torch.nn.Module,
                          prefix: str) -> Optional["QuantizeMethodBase"]:
